d9a3dc736349d8cf3f783d4e401d573481073592
sachin-saurabh <sachin.srbh@gmail.com>
2021-02-07 14:28:07 +0530

    code commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let array = [1,2,3,4,5,6,7,8,9];  // array of input
+let sortBtn = document.getElementById('sortBtn');
+let suffleBtn = document.getElementById('suffleBtn');
+
+/**
+ * 
+ * @param {*} arr : takes in input of array and generates grid with style
+ */
+let cardGenerator = (arr) => {
+    document.getElementById('g-container').innerHTML = '';
+    for (i in arr) {
+        let numChild = document.createElement('div');
+        numChild.id = 'num_'+i;
+        numChild.className = 'cell'; 
+        let clientWidth = document.documentElement.clientWidth;
+        let color = '';
+         switch(arr[i].toString()) {
+            case '1':
+            case '8': color = '#6F98A8';
+            break;
+            case '3':case '5':case '9': color = '#2F454E';
+            break;
+            case '2': case '4':color = '#2B8EAD';
+            break;
+            case '6': case '7': color = '#BFBFBF';
+            break;
+        }
+        if (clientWidth < 376) {
+            numChild.style.backgroundColor = '#EFEFEF';
+            numChild.style.borderLeft = '5px solid '+ color;
+        } else {
+            numChild.style.backgroundColor = color; 
+            numChild.style.borderLeft = '0px';
+        }
+        numChild.innerHTML = arr[i];
+        document.getElementById('g-container').appendChild(numChild);
+    }
+}
+
+window.onload = () => {
+    let titleMsg = 'Shuffle and Sort';
+    let creator = 'by Sachin Saurabh';
+    document.getElementById('header').innerHTML = titleMsg;
+    document.getElementById('footer').innerHTML = titleMsg + creator;
+    cardGenerator(array);
+};
+
+window.addEventListener('resize', () => {
+    cardGenerator(array);
+});
+
+sortBtn.addEventListener('click', () => {
+    array = array.sort();
+    cardGenerator(array);
+});
+
+suffleBtn.addEventListener('click', () => {
+    array = randomiser(array);
+    cardGenerator(array);
+});
+
+/**
+ * 
+ * @param {*} arr  on click of shuffle button takes array and shuffles the index of the array data.
+ */
+let randomiser = (arr) => {
+    const newArr = [];
+    for ( let i = arr.length-1; i>=0; i--) {
+        let index = Math.floor(Math.random()*(i));
+        [arr[i], arr[index]] = [arr[index], arr[i]];
+    }
+   return arr;
+}
